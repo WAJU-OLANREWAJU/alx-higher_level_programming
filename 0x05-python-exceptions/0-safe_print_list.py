77be2a10dde4d9b12a5aa02a2e78b7ad5b9f3fb7
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-03-29 07:50:43 -0700

    python exception task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    n = 0
-    for m in range(x):
-        try:
-            print(my_list[m], end="")
-            n += 1
-        except IndexError:
-            break
-    print()
-    return n
+safe_print_list = __import__('0-safe_print_list').safe_print_list
 
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
