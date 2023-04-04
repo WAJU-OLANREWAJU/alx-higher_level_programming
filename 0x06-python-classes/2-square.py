3d7eef0423f60668732fe8ecc6fe528954234d21
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-04-04 06:12:33 -0700

    solution to ask 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' A class that defines a square with exception handling '''
+
+class Square:
+    ''' A Square class defined '''
+    def __init__(self, size):
+        """Initializing this square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is < zero
+        """
+
+        if not isinstance(size, int):
+                raise TypeError ("size must be an integer")
+
+        if size < 0:
+                raise ValueError("size must be >= 0")
+        
+        self.__size = size
