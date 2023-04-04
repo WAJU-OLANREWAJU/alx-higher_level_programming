b8249689256286cd0aa734e13ceb5964a845b168
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-04-03 18:43:10 -0700

    my solution to classes and object task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#1. Square with size by Ibrahim
+''' Dfines a square class with a private instance(size) as an attribute '''
+
+class Square:
+    ''' This private instance the size attribute '''
+    def __init__ (self, size):
+        self.__size = size
+
