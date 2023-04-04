30cc1b932ced09571958467343b0ba6b85a9cb93
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-04-03 17:50:41 -0700

    My solution to object and classes task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,11 @@
 #!/usr//bin/python3
 
-''' This is an empty square class '''
-
 class Square:
+''' This is an empty square class '''
     pass
 
 
 
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+#my_square = Square()
+#print(type(my_square))
+#print(my_square.__dict__)
