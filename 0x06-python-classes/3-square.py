44b96189e573ee999c296c73de59456eb842d8c6
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-04-04 08:55:08 -0700

    my solution to task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' A class that defines a square with exception handling '''
+
+class Square:
+    ''' A Square class defined '''
+    def __init__(self, size):
+        """Initializing this square class
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is < zero
+        """
+    
+        if not isinstance(size, int):
+                raise TypeError ("size must be an integer")
+
+        if size < 0:
+                raise ValueError("size must be >= 0")
+
+        self.__size = size
+    @property
+        '''this allows the size attribute to be accesible later in the coe '''
+    def size(self):
+            return self.__size
+
+    def area(self):
+        ''' this metod returns the area of the square '''
+        return int(self.size) * int(self.size)
