e8ae3770ba98a54fe19683e838111064f997a237
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-04-05 07:03:12 -0700

    My solution to task 4 in OOP project

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,27 @@
-class Square:
+#!/usr/bin/python3
+''' A Square class definition '''
 
+class Square:
+    ''' Initializing the class attributes and privatizing size '''
     def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
-      return self.__size
+        ''' defines a getter to retrieve the private instance attribute '''
+        return self.__size
 
     @size.setter
     def size(self, value):
+        '''
+    defines the setter to control the input of the private instanc attribute
+    args:
+        value: modifies the private instance attribute
+    checks:
+        Typeerror: value must be an integer
+        Valueerror: value must < 0
+    '''
+
         if not isinstance(value, int):
             raise TypeError ("size must be an integer")
         elif value < 0:
@@ -17,5 +30,6 @@ class Square:
             self.__size = value
     
     def area(self):
+        ''' returns the square area '''
         return int(self.__size) * int(self.__size)
 
