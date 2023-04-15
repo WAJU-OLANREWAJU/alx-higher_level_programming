5dfa9f9cc4deddb417d57dcab817d2b79717a790
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-04-15 15:22:07 -0700

    My solution to task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 
-'''
+
 class MyList(list):
+    ''' My list class '''
+
     def __init__(self, *args, **kwargs):
+        ''' Modification that sorts the list '''
         super().__init__(*args, **kwargs)
 
     def print_sorted(self):
+        ''' this sorts '''
         self.sort()
-        print(self)
-        '''
-class MyList(list):
-
-    ''' My list class '''
-
-    def print_sorted(self):
-
-        ''' Modification to sort the list '''
-        
-        original_list = self.copy()  # Create a copy of the original list
-        sorted_list = sorted(original_list)  # Sort the copy
-        print(original_list)
-        print(sorted_list)
         print(self)
