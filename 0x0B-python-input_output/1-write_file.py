a9ff120c25182f1659e00bfd984231b24936c2dd
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-04-22 00:59:54 -0700

    My solution to task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """
     with open(filename, 'w', encoding='utf-8') as file:
         nb_char = file.write(text)
-        return (nb_ char)
+        return nb_ char
