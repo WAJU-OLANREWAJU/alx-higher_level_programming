6f33993c44b5ba6d7d36df050edaad7a26fde67a
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-04-21 23:58:56 -0700

    My solution to task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module reads a text file """
+
+def read_file(filename=""):
+    """ A function definition that reads a text file and prints to stdout
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content)
