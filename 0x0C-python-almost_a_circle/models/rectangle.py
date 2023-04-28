f60a75b208cea46beb2a36976afd7be8a896108b
WAJU-OLANREWAJU <waju_olanrewaju@gmail.com>
2023-04-28 05:24:50 -0700

    My solution to task 0 and 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,43 +18,42 @@ class Rectangle(Base):
         self.__y = y
 
         @property
-        # funtion to get private instance
         def width(self):
+            """ funtion to get private instance """
             pass
-        
+
         @width.setter
-        # function to set private instance
         def width(self, value):
+            """ setter for private instance """
             pass
 
         @property
-        # funtion to get private instance
         def height(self):
             pass
 
         @height.setter
-        # function to set private instance
         def height(self, value):
+            """ setter for private instance """
             pass
 
         @property
-        # funtion to get private instance
         def x(self):
+            """ funtion to get private instance """
             pass
 
         @x.setter
-        # function to set private instance
         def x(self, value):
+            """ setter for private instance """
             pass
 
         @property
-        # funtion to get private instance
         def y(self):
+            """ funtion to get private instance """
             pass
 
         @y.setter
-        # function to set private instance
         def y(self, value):
+            """ setter for private instance """
             pass
 
 
