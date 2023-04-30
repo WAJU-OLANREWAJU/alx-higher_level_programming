4f9af841a9e02a8a52de090e8c3851115ab0c911
WAJU-OLANREWAJU <waju_olanrewaju@gmail.com>
2023-04-30 07:43:33 -0700

    My solution to task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,14 +100,14 @@ class Rectangle(Base):
     def update(self, *args):
         """ This method assigns an argument to each at a specified position
         """
-        if len(args) == 1:
+        if len(args) >= 1:
             self.id = args[0]
-        if len(args) == 2:
+        if len(args) >= 2:
             self.__width = args[1]
-        if len(args) == 3:
+        if len(args) >= 3:
             self.__height = args[2]
-        if len(args) == 4:
+        if len(args) >= 4:
             self.__x = args[3]
-        if len(args) == 5:
+        if len(args) >= 5:
             self.__y = args[4]
 
