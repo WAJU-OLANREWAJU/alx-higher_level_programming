14a02f1ba70c785a6da070bfeadbd44bd5b72694
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-06-08 03:10:13 -0700

    correction to task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,16 +2,20 @@
 """A class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
 """
 class Rectangle:
+    """A class named Rectangle"""
     
     def __init__(self, width=0, height=0):
+        """Constructor method, defines two private atributes"""
         self.__width = width
         self.__height = height
     
     @property
+    """allows for retrieval of the private attribute"""
     def width(self):
         return self.__width
     
     @width.setter
+    """allows for setting private attributes"""
     def width(self, width):
         self.__width = width
         if not isinstance(self.__width, int):
@@ -20,10 +24,12 @@ class Rectangle:
             raise ValueError('width must be >= 0')
     
     @property
+    """allows for retrieval of the private attribute"""
     def height(self):
         return self.__height
     
     @height.setter
+    """allows for setting private attributes"""
     def height(self, height):
         self.__height = height
         if not isinstance(self.__height, int):
