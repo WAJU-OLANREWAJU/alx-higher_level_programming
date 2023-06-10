c68293b89632922b161790538da5a001d146a0e2
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-06-10 00:48:18 -0700

    correction to task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -92,7 +92,7 @@ class Rectangle:
             output = ""
             for i in range(self.__height):
                 for j in range(self.__width):
-                    output += self.print_symbol
+                    output += str(self.print_symbol)
                 if i < self.__height -1:
                     output += "\n"
 
